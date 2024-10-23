17b711f014c31e33f97240defaa2a2d959d9e6ac
casiojapi <f91w@tuta.io>
2024-10-23 03:27:40 -0300

    fix url stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: '0.0.0.0', // Expose on all interfaces
+    port: 3000,
+    hmr: {
+      clientPort: 443, // Ensure HMR works with Ngrok HTTPS
+    },
+  },
+});
+
+
+
+
+
+
+
+
+
